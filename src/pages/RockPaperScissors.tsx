0b73ccde163ff9ac9332b86fce1b059407047334
boyarkogc <boyarko.greg@gmail.com>
2025-03-22 14:59:00 -0600

    added initial elements

--- src/pages/RockPaperScissors.tsx
+++ src/pages/RockPaperScissors.tsx
@@ -1,9 +1,16 @@
 export default function RockPaperScissors() {
   return (
-    <div>
-      <h1>Rock Paper Scissors</h1>
-      <p>Welcome to the Rock Paper Scissors game!</p>
-      <p>Choose your move and see if you can beat the computer!</p>
-    </div>
+    <>
+      <div>
+        <h1>Rock Paper Scissors</h1>
+        <p>Welcome to the Rock Paper Scissors game!</p>
+        <p>Choose your move and see if you can beat the computer!</p>
+      </div>
+      <div className="flex">
+        <button>Rock</button>
+        <button>Paper</button>
+        <button>Scissors</button>
+      </div>
+    </>
   )
 }
