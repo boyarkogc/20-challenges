8d2ce929ab28163c6f7fd2539a27a00248d2b8e7
Greg Boyarko <boyarko.greg@gmail.com>
2025-03-22 14:25:19 -0600

    rock paper scissors initial commit

---
+++ src/pages/RockPaperScissors.tsx
@@ -0,0 +1,9 @@
+export default function RockPaperScissors() {
+  return (
+    <div>
+      <h1>Rock Paper Scissors</h1>
+      <p>Welcome to the Rock Paper Scissors game!</p>
+      <p>Choose your move and see if you can beat the computer!</p>
+    </div>
+  )
+}
