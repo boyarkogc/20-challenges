a1c297e46877aa5047a6db88f732d3431f33bd90
Greg Boyarko <boyarko.greg@gmail.com>
2025-03-22 20:47:07 -0600

    game is now working

--- src/pages/RockPaperScissors.tsx
+++ src/pages/RockPaperScissors.tsx
@@ -1,4 +1,16 @@
+import { useState } from 'react'
+import Card from '../components/RockPaperScissors/Card'
+
 export default function RockPaperScissors() {
+  const [gameState, setGameState] = useState<{playerMove: string, computerMove: string}>({playerMove: "", computerMove: ""})
+  const setPlayerMove = (playerMove: string): void => {
+    const rand = Math.floor(Math.random() * 3)
+    const computerMove = rand === 0 ? "Rock"
+      : rand === 1 ? "Paper"
+      : "Scissors"
+    setGameState({playerMove, computerMove})
+  }
+
   return (
     <>
       <div>
@@ -6,10 +18,13 @@ export default function RockPaperScissors() {
         <p>Welcome to the Rock Paper Scissors game!</p>
         <p>Choose your move and see if you can beat the computer!</p>
       </div>
+      {gameState.playerMove ? 
+        <div>{gameState.playerMove} - {gameState.computerMove}</div> 
+      : null}
       <div className="flex">
-        <button>Rock</button>
-        <button>Paper</button>
-        <button>Scissors</button>
+        <Card move="Rock" onClick={() => setPlayerMove("Rock")} />
+        <Card move="Paper" onClick={() => setPlayerMove("Paper")} />
+        <Card move="Scissors" onClick={() => setPlayerMove("Scissors")} />
       </div>
     </>
   )
